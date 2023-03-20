90ebe2d3f7ae7716cac4afd5ab3144305cec7009
tan <buitan561993@gmail.com>
2023-03-20 20:18:49 +0700

    update

--- src/components/Home-component/Header.js
+++ src/components/Home-component/Header.js
@@ -30,7 +30,7 @@ const Header = () => {
                     <div className='header-supportPhoneNumber'>
                         <span> {phoneIcon}</span><p > CSKH 24/7 :<span style={{fontWeight:"bold"}}> 0921881333</span></p>
                     </div>
-                    <div className='header-menu-icon'>
+                    <div className='header-menu-icon '>
                         <button className="relative group">
                             <div className="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-purple-700 ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
                                 <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
